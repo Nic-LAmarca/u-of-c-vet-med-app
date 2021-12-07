3d18813029a2514dd6564689856fd97eda16e5af
calvingiese <calvin.giese@ucalgary.ca>
2021-12-06 18:43:26 -0700

    Front end for animal profile student page.

--- api607/src/main/ui/src/AnimalProfileStudent.js
+++ api607/src/main/ui/src/AnimalProfileStudent.js
@@ -9,138 +9,86 @@ import DropdownItem from "react-bootstrap/DropdownItem";
 
 
 export default function AnimalProfileStudentDashboard() {
-    // let [animals,setAnimals] = useState([]);
-    // const [requestAnimalId, setRequestAnimalId] = useState();
-    // const [requestAnimal, setRequestAnimal] = useState('');
-    // let [requests,setRequests] = useState([]);
-    // const [cancelRequestId, setCancelRequestId] = useState();
-    // const [cancelRequest, setCancelRequest] = useState('');
-    //
-    //
-    // const history = useNavigate();
-    //
-    // async function handleLogout(event) {
-    //     event.preventDefault();
-    //     history('/');
-    // }
-    //
-    // // *** All for Animal ***
-    // async function dropDown(event) {
-    //         const json = {
-    //         };
-    //         await axios.get('http://localhost:8080/NavAAvailableAnimals', json,
-    //         {
-    //             headers: {
-    //             'content-type': 'application/json'
-    //             }
-    //         })
-    //         .then(function(response){
-    //             setAnimals(response.data);
-    //         })
-    //         .catch(function(error){
-    //             console.log(error);
-    //         });
-    //     }
-    //
-    //     async function handleSelect(event){
-    //         event.preventDefault();
-    //         event.persist();
-    //         setRequestAnimalId(event.target.text);
-    //     }
-    //
-    // async function makeRequest(event) {
-    //     event.preventDefault();
-    //     await axios.post('http://localhost:8080/AnimalPDashboard',
-    //         null,
-    //         {
-    //             params: {
-    //                 requestAnimalId
-    //             }
-    //         })
-    //         .then(function(response){
-    //             setRequestAnimal(response.data);
-    //         })
-    //         .catch(function(error){
-    //             console.log(error);
-    //         });
-    // }
-    // // *** All for Animal ***
-    //
-    // // *** All for Request ***
-    // async function cancelDropDown(event) {
-    //         const json = {
-    //         };
-    //         await axios.get('http://localhost:8080/NavAAvailableRequests', json,
-    //         {
-    //             headers: {
-    //             'content-type': 'application/json'
-    //             }
-    //         })
-    //         .then(function(response){
-    //             setRequests(response.data);
-    //         })
-    //         .catch(function(error){
-    //             console.log(error);
-    //         });
-    //     }
-    //
-    //     async function handleCancelSelect(event){
-    //         event.preventDefault();
-    //         event.persist();
-    //         setCancelRequestId(event.target.text);
-    //     }
-    //
-    // async function CancelRequest(event) {
-    //     event.preventDefault();
-    //     await axios.post('http://localhost:8080/AnimalPashboard',
-    //         null,
-    //         {
-    //             params: {
-    //                 cancelRequestId
-    //             }
-    //         })
-    //         .then(function(response){
-    //             setCancelRequest(response.data);
-    //         })
-    //         .catch(function(error){
-    //             console.log(error);
-    //         });
-    // }
-    // // *** All for Request ***
-
     return (
-
         <div className="AnimalP-grid-container">
             <h3 className="AnimalP-grid-item1">
-                Animal A
+                Sparky
             </h3>
             <h3 className="AnimalP-grid-item2">
-                ID#
+                ID # 1
             </h3>
             <h3 className="AnimalP-grid-item3">
-                Animal Status
+                Status: Healthy
             </h3>
             <Table striped bordered hover className="AnimalP-grid-item5">
                 <tbody>
                 <tr>
-                    <td>Name</td>
-                    <td>Animal A</td>
-
+                    <td>ID# </td>
+                    <td>1</td>
                 </tr>
                 <tr>
-                    <td>ID# </td>
-                    <td>1234567</td>
-
+                    <td>Name</td>
+                    <td>Sparky</td>
+                </tr>
+                <tr>
+                    <td>Species</td>
+                    <td>Dog</td>
                 </tr>
                 <tr>
                     <td>Weight</td>
-                    <td>11.08 Kg</td>
-
+                    <td>3.6Kg</td>
                 </tr>
                 <tr>
-                    <td>Tattoo#</td>
-                    <td> 7654321</td>
-
+                    <td>Tattoo Number</td>
+                    <td>234234</td>
+                </tr>
+                <tr>
+                    <td>City Tattoo</td>
+                    <td>HOC London</td>
+                </tr>
+                <tr>
+                    <td>Birth Date</td>
+                    <td>2018-08-15</td>
+                </tr>
+                <tr>
+                    <td>Breed</td>
+                    <td>Beagle</td>
+                </tr>
+                <tr>
+                    <td>Sex</td>
+                    <td>MN</td>
+                </tr>
+                <tr>
+                    <td>RFID</td>
+                    <td>197839178371</td>
+                </tr>
+                <tr>
+                    <td>Microchip</td>
+                    <td>176387613813</td>
+                </tr>
+                <tr>
+                    <td>Status</td>
+                    <td>Healthy</td>
+                </tr>
+                <tr>
+                    <td>Available</td>
+                    <td>true</td>
+                </tr>
+                <tr>
+                    <td>Purpose</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Region</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Subspecies</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Color</td>
+                    <td>Brown, black, White</td>
                 </tr>
                 </tbody>
             </Table>
@@ -149,58 +97,132 @@ export default function AnimalProfileStudentDashboard() {
                 <div className="AnimalP-grid-item7">
                 <Nav variant="pills"  >
                     <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        <Nav.Link eventKey="first">Comments</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        <Nav.Link eventKey="second">Photos</Nav.Link>
                     </Nav.Item>
-
+                    <Nav.Item>
+                        <Nav.Link eventKey="third">Diagnoses</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="fourth">Prescriptions</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="fifth">Alerts</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="sixth">Requests</Nav.Link>
+                    </Nav.Item>
                 </Nav>
                 </div>
                 <div className="AnimalP-grid-item6">
                     <Tab.Content >
                         <Tab.Pane eventKey="first">
-                            hi hello
+                            <Table striped bordered hover className="AnimalP-grid-item100">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">commentId</th>
+                                        <th scope="col">User Name</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Calvin</td>
+                                        <td>Sparky looks healthy</td>
+                                        <td>2021-11-24</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
                         </Tab.Pane>
                         <Tab.Pane eventKey="second">
-                            hello hello
-                            dont stop believing
-                            seasoning is the reason for the season
-                            hello hello
-                            dont stop believing
-                            seasoning is the reason for the season
-                            hello hello
-                            dont stop believing
-                            seasoning is the reason for the season
                         </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                            <Table striped bordered hover className="AnimalP-grid-item100">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">commentId</th>
+                                        <th scope="col">User Name</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Calvin</td>
+                                        <td>Sparky looks healthy</td>
+                                        <td>2021-11-24</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="fourth">
+                        <Table striped bordered hover className="AnimalP-grid-item100">
+                            <thead>
+                                <tr>
+                                    <th scope="col">commentId</th>
+                                    <th scope="col">User Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Calvin</td>
+                                    <td>Sparky looks healthy</td>
+                                    <td>2021-11-24</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="fifth">
+                        <Table striped bordered hover className="AnimalP-grid-item100">
+                            <thead>
+                                <tr>
+                                    <th scope="col">commentId</th>
+                                    <th scope="col">User Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Calvin</td>
+                                    <td>Sparky looks healthy</td>
+                                    <td>2021-11-24</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="sixth">
+                        <Table striped bordered hover className="AnimalP-grid-item100">
+                            <thead>
+                                <tr>
+                                    <th scope="col">commentId</th>
+                                    <th scope="col">User Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Calvin</td>
+                                    <td>Sparky looks healthy</td>
+                                    <td>2021-11-24</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Tab.Pane>
                     </Tab.Content>
                 </div>
             </Tab.Container>
-            {/*<Tab.Container id="left-tabs-example" defaultActiveKey="first" >*/}
-            {/*    <Row className="AnimalP-grid-item7">*/}
-            {/*        <Col sm={3}>*/}
-            {/*            <Nav variant="pills" className="flex-column">*/}
-            {/*                <Nav.Item>*/}
-            {/*                    <Nav.Link eventKey="first">Tab 1</Nav.Link>*/}
-            {/*                </Nav.Item>*/}
-            {/*                <Nav.Item>*/}
-            {/*                    <Nav.Link eventKey="second">Tab 2</Nav.Link>*/}
-            {/*                </Nav.Item>*/}
-            {/*            </Nav>*/}
-            {/*        </Col>*/}
-            {/*        <Col sm={9}>*/}
-            {/*            <Tab.Content>*/}
-            {/*                <Tab.Pane eventKey="first">*/}
-            {/*                    hi hello*/}
-            {/*                </Tab.Pane>*/}
-            {/*                <Tab.Pane eventKey="second">*/}
-            {/*                    hello hello*/}
-            {/*                </Tab.Pane>*/}
-            {/*            </Tab.Content>*/}
-            {/*        </Col>*/}
-            {/*    </Row>*/}
-            {/*</Tab.Container>*/}
-
         </div>
     );
 }
